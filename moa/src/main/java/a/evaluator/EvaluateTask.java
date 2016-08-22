40d7f5142bb33abf66a276f0dff4231946f36422
burnmg <rlin1992@hotmail.com>
2016-08-23 10:34:51 +1200

    t

--- moa/src/main/java/a/evaluator/EvaluateTask.java
+++ moa/src/main/java/a/evaluator/EvaluateTask.java
@@ -1,27 +1,23 @@
 package a.evaluator;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.concurrent.Callable;
-
-import javax.sound.sampled.Line;
-import javax.swing.text.Highlighter.Highlight;
-
-import org.apache.poi.ss.formula.functions.Sumif;
-
 import a.tools.Directory;
 
 import moa.classifiers.AbstractClassifier;
 import moa.classifiers.a.VolatilityAdaptiveClassifer;
 import moa.streams.ExampleStream;
 import moa.tasks.StandardTaskMonitor;
-import weka.gui.beans.Startable;
 
 public class EvaluateTask implements Callable<Integer>
 {
@@ -248,6 +244,27 @@ public class EvaluateTask implements Callable<Integer>
 
 		}
 		return result;
+	}
+	
+	private int countLines(String filename) throws IOException {
+	    InputStream is = new BufferedInputStream(new FileInputStream(filename));
+	    try {
+	        byte[] c = new byte[1024];
+	        int count = 0;
+	        int readChars = 0;
+	        boolean empty = true;
+	        while ((readChars = is.read(c)) != -1) {
+	            empty = false;
+	            for (int i = 0; i < readChars; ++i) {
+	                if (c[i] == '\n') {
+	                    ++count;
+	                }
+	            }
+	        }
+	        return (count == 0 && !empty) ? 1 : count;
+	    } finally {
+	        is.close();
+	    }
 	}
 
 
